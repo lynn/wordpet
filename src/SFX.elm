e7d5555f21062aee9c2cb274833fbc7313a14fb5
Lynn <lynn@foldr.moe>
2017-09-16 17:58:25 +0200

    rename sfx, add chomps

--- src/SFX.elm
+++ src/SFX.elm
@@ -3,22 +3,21 @@ import Audio
 
 type SFX
   = Chirp
-  | Beep
+  | Chomp
 
 chirps : List String
 chirps =
-  [ "assets/sfx/10.mp3"
-  , "assets/sfx/11.mp3"
-  , "assets/sfx/12.mp3" ]
+  [ "assets/sfx/tweet0.mp3"
+  , "assets/sfx/tweet1.mp3"
+  , "assets/sfx/tweet2.mp3" ]
 
-beeps : List String
-beeps =
-  [ "assets/sfx/20.mp3"
-  , "assets/sfx/21.mp3"
-  , "assets/sfx/22.mp3" ]
+chomps : List String
+chomps =
+  List.range 0 119
+  |> List.map (\i -> "assets/sfx/chomp" ++ toString i ++ ".mp3")
 
 play : SFX -> Cmd msg
 play sfx =
   case sfx of
     Chirp -> Audio.playOneOf chirps
-    Beep -> Audio.playOneOf beeps
+    Chomp -> Audio.playOneOf chomps
